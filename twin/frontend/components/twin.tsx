bc3af7894fcc5239dfb288e53e1946f88b453f64
NachoBosch <nachobosch5@gmail.com>
2026-01-13 16:01:35 -0300

    digital twin deployed in aws

--- twin/frontend/components/twin.tsx
+++ twin/frontend/components/twin.tsx
@@ -40,7 +40,7 @@ export default function Twin() {
         setIsLoading(true);
 
         try {
-            const response = await fetch('http://localhost:8000/chat', {
+            const response = await fetch('https://b73vno5tbi.execute-api.sa-east-1.amazonaws.com/chat', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
